237d717464d67abd5c28c0db1ededf29cc671d7b
karola65 <54160424+karola65@users.noreply.github.com>
2019-10-22 09:20:09 +0800

    Update User.java

--- PayMeBack/src/main/java/g1/paymeback/User.java
+++ PayMeBack/src/main/java/g1/paymeback/User.java
@@ -4,10 +4,71 @@ package g1.paymeback;
  *
  * @author Karolina
  */
+
+
+/**
+ *
+ * User of application
+ */
 public class User {
-    private int id;
     
-    public int getID(){
-        return this.id;
-    }
+    String userName;
+    String firstName;
+    String lastName;
+    String password;
+    int id;
+
+   public User(String firstName, String lastName, String email, String userName, String password)
+  {
+      this.firstName= firstName;
+      this.lastName = lastName;
+      this.userName = userName;
+      this.password = password;
+   
+      
+  }
+  
+  public User()
+  {
+      
+  }
+
+  public String getFirstName()
+  {
+    return this.firstName;
+  }
+  public String getLastName()
+  {
+    return this.lastName;
+  }
+
+  public String getUserName()
+  {
+    return this.userName;
+  }
+  public String getPassword()
+  {
+    return this.password;
+  }
+
+  public void setFirstName(String firstName)
+  {
+    this.firstName = firstName;
+  }
+  public void setLastName(String lastName)
+  {
+    this.lastName = lastName;
+  }
+  public void setUserName(String userName)
+  {
+    this.userName = userName;
+  }
+  public void setPassword(String password)
+  {
+    this.password = password;
+  }
+  
+
 }
+
+
